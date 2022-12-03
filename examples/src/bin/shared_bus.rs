0b93155728fc6fb1ea3280101b22fd9aa76dd00c
kalkyl <henrik.alser@me.com>
2022-12-04 00:53:32 +0100

    Async-fn-in-traits

--- examples/src/bin/shared_bus.rs
+++ examples/src/bin/shared_bus.rs
@@ -6,25 +6,28 @@
 
 use nrf_embassy as _; // global logger + panicking-behavior
 
+use core::f32::consts::PI;
 use defmt::*;
-use embassy_executor::executor::Spawner;
-use embassy_executor::time::{Delay, Duration, Timer};
-use embassy_util::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_util::{mutex::Mutex, Forever};
-use embassy_nrf::twim::{self, Twim};
-use embassy_nrf::{interrupt, Peripherals, peripherals::TWISPI0};
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
+use embassy_executor::Spawner;
+use embassy_executor::_export::StaticCell;
+use embassy_nrf::twim::{self, Twim};
+use embassy_nrf::{interrupt, peripherals::TWISPI0};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::{Delay, Duration, Timer};
+use libm::atan2;
 use mpu6050_async::*;
 use qmc5883l_async::*;
-use core::f32::consts::PI;
-use libm::atan2;
 
 // Need correct magnetic declination for your location for accurate
 // readings. See http://www.magnetic-declination.com/
 const DECLINATION_RADS: f32 = 0.024434609;
 
 #[embassy_executor::task]
-async fn compass_task(mut compass: QMC5883L<I2cDevice<'static, ThreadModeRawMutex, Twim<'_, TWISPI0>>>) {
+async fn compass_task(
+    mut compass: QMC5883L<I2cDevice<'static, ThreadModeRawMutex, Twim<'_, TWISPI0>>>,
+) {
     compass.continuous().await.unwrap();
     loop {
         if let Ok((x, y, z)) = compass.mag().await {
@@ -56,19 +59,20 @@ async fn gyro_task(mut mpu: Mpu6050<I2cDevice<'static, ThreadModeRawMutex, Twim<
 }
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner, p: Peripherals) {
-    static I2C_BUS: Forever<Mutex::<ThreadModeRawMutex, Twim<TWISPI0>>> = Forever::new();
+async fn main(spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    static I2C_BUS: StaticCell<Mutex<ThreadModeRawMutex, Twim<TWISPI0>>> = StaticCell::new();
     let config = twim::Config::default();
     let irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
     let i2c = Twim::new(p.TWISPI0, irq, p.P0_03, p.P0_04, config);
     let i2c_bus = Mutex::<ThreadModeRawMutex, _>::new(i2c);
-    let i2c_bus = I2C_BUS.put(i2c_bus);
+    let i2c_bus = I2C_BUS.init(i2c_bus);
 
     let i2c_dev1 = I2cDevice::new(i2c_bus);
     let compass = QMC5883L::new(i2c_dev1).await.unwrap();
     unwrap!(spawner.spawn(compass_task(compass)));
-    
+
     let i2c_dev2 = I2cDevice::new(i2c_bus);
-    let mpu = Mpu6050::new(i2c_dev2); 
+    let mpu = Mpu6050::new(i2c_dev2);
     unwrap!(spawner.spawn(gyro_task(mpu)));
 }
--- examples/src/bin/simple.rs
+++ examples/src/bin/simple.rs
@@ -8,14 +8,15 @@
 use nrf_embassy as _; // global logger + panicking-behavior
 
 use defmt::*;
-use embassy_executor::executor::Spawner;
-use embassy_executor::time::{Delay, Duration, Timer};
+use embassy_executor::Spawner;
+use embassy_nrf::interrupt;
 use embassy_nrf::twim::{self, Twim};
-use embassy_nrf::{interrupt, Peripherals};
+use embassy_time::{Delay, Duration, Timer};
 use mpu6050_async::*;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner, p: Peripherals) {
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
     let config = twim::Config::default();
     let irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
     let i2c = Twim::new(p.TWISPI0, irq, p.P0_03, p.P0_04, config);
